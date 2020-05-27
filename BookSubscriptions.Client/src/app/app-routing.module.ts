1d550906bd9bc8e5e12220015f37f129039d9863
Ryan Richard <ryan.richard@derivco.co.za>
2020-05-27 18:36:55 +0200

    fixed routing

--- BookSubscriptions.Client/src/app/app-routing.module.ts
+++ BookSubscriptions.Client/src/app/app-routing.module.ts
@@ -4,38 +4,27 @@ import { HomeComponent } from './home/home.component';
 import { RegistrationFormComponent } from './account/registration-form/registration-form.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { LoginFormComponent } from './account/login-form/login-form.component';
-import { BooksComponent } from './books/books.component';
 import { SelectiveStrategy } from './selective-strategy.service';
-
-
-const routes: Routes = [
-
-  { path: 'home', component: HomeComponent },
-  {
-    path: 'books',
-    data: { preload: false },
-    loadChildren: () =>
-      import('./books/books.module').then(m => m.BooksModule)
-  },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent }
-];
-
 
 @NgModule({
   imports: [
-    RouterModule.forRoot([
-      { path: 'home', component: HomeComponent },
-      {
-        path: 'books',
-        data: { preload: false },
-        loadChildren: () =>
-          import('./books/books.module').then(m => m.BooksModule)
-      },
-      { path: '', redirectTo: '/home', pathMatch: 'full' },
-      { path: '**', component: PageNotFoundComponent }
-      ], { enableTracing: true, preloadingStrategy: SelectiveStrategy })
-],
-exports: [RouterModule]
+    RouterModule.forRoot(
+      [
+        { path: 'home', component: HomeComponent },
+        { path: 'register', component: RegistrationFormComponent },
+        { path: 'login', component: LoginFormComponent },
+        {
+          path: 'books',
+          data: { preload: false },
+          loadChildren: () =>
+            import('./books/books.module').then((m) => m.BooksModule),
+        },
+        { path: '', redirectTo: '/home', pathMatch: 'full' },
+        { path: '**', component: PageNotFoundComponent },
+      ],
+      { enableTracing: true, preloadingStrategy: SelectiveStrategy }
+    ),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
